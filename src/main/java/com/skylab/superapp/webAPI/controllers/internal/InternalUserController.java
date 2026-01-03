af0a9863cf798deff948f56884c099dbf2c08cfb
yustyy <yusuf.acmaci@hotmail.com>
2026-01-03 21:25:32 +0300

    feat: add InternalUserController for user management endpoints

---
+++ src/main/java/com/skylab/superapp/webAPI/controllers/internal/InternalUserController.java
@@ -0,0 +1,48 @@
+package com.skylab.superapp.webAPI.controllers.internal;
+
+import com.skylab.superapp.business.abstracts.UserService;
+import com.skylab.superapp.core.constants.UserMessages;
+import com.skylab.superapp.core.results.DataResult;
+import com.skylab.superapp.core.results.SuccessDataResult;
+import com.skylab.superapp.entities.DTOs.User.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/internal/api/users")
+public class InternalUserController {
+
+
+    private final UserService userService;
+
+
+    public InternalUserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/authenticated-user")
+    public ResponseEntity<DataResult<UserDto>> getAuthenticatedUser() {
+        var result = userService.getAuthenticatedUser();
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new SuccessDataResult<>(result, UserMessages.USER_GET_SUCCESS,
+                        HttpStatus.OK));
+    }
+
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DataResult<UserDto>> getUserById(@PathVariable UUID id) {
+        var result = userService.getUserById(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new SuccessDataResult<>(result, UserMessages.USER_GET_SUCCESS, HttpStatus.OK));
+    }
+
+
+
+}
